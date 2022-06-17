cca7254b81bb14fa26410209ac56dd766ad6e7b4
Alerinnan <107584547+Alerinnan@users.noreply.github.com>
2022-06-17 12:29:41 +0300

    Update study.sql

--- study.sql
+++ study.sql
@@ -2,3 +2,14 @@ SELECT 'Я начинаю изучать SQL' -- напишите запрос �
 
 SELECT * -- напишите запрос тут
 FROM flights;
+
+SELECT COUNT(flights.flight_id) AS flights_amount,
+       CASE
+           WHEN model LIKE 'Airbus%' THEN 'Airbus'
+           WHEN model LIKE 'Boeing%' THEN 'Boeing'
+           ELSE 'other'
+       END AS type_aircraft
+FROM aircrafts
+INNER JOIN flights ON flights.aircraft_code = aircrafts.aircraft_code
+WHERE CAST(flights.departure_time AS date) BETWEEN '2018-09-01' AND '2018-09-30'
+GROUP BY type_aircraft; 
